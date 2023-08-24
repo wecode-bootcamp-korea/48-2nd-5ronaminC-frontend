30a3a75034ade6d1b4bbca0828de8882a8f14dd6
serenity1091 <seulki1091@gmail.com>
2023-08-24 21:13:33 +0900

    Add : cart페이지 Layout 구성 중

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+//import Nav from '../../components/Nav/Nav';
+// import Footer from '../../components/Footer/Footer';
+// import CartList from './components/cartList';
+import ProductPriceAll from './components/productPriceAll';
 import './Cart.scss';
 
 const Cart = () => {
-  return <>나는 장바구니</>;
+  return (
+    <>
+      {/* <Nav /> */}
+      <div className="cart">
+        <div className="cartAll">
+          <div></div>
+          {/* <div className="cartList">
+            <CartList />
+          </div> */}
+        </div>
+        <div className="productPrice">
+          <div></div>
+          <div className="productPriceAll">
+            <ProductPriceAll />
+          </div>
+        </div>
+      </div>
+      {/* <Footer /> */}
+    </>
+  );
 };
-
 export default Cart;
