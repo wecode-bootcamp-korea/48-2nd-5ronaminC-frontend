ae95899712b0aa2cada403c9d509a78599532c41
serenity1091 <seulki1091@gmail.com>
2023-08-24 22:24:56 +0900

    Add: Payment  레이아웃 작업 중

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+//import Nav from '../../components/Nav/Nav';
+// import Footer from '../../components/Footer/Footer';
+// import CartList from './components/cartList';
 import './Payment.scss';
 
 const Payment = () => {
-  return <>나는 결제</>;
+  return (
+    <>
+      {/* <Nav /> */}
+      <div className="cart">
+        <div className="cartAll">
+          <div></div>
+          {/* <div className="cartList">
+            <CartList />
+          </div> */}
+        </div>
+        <div className="productPrice">
+          {/* <div></div>
+          <div className="productPriceAll">
+            <ProductPriceAll />
+          </div> */}
+        </div>
+      </div>
+      {/* <Footer /> */}
+    </>
+  );
 };
-
 export default Payment;
