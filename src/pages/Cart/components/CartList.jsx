673a43cf79762579e181431935a7b2349d4ba30e
serenity1091 <seulki1091@gmail.com>
2023-08-24 22:37:43 +0900

    Add : cart페이지 Layout 구성 중

---
+++ src/pages/Cart/components/CartList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './CartList.scss';
+
+const CartList = () => {
+  return (
+    <div className="CartList">
+      <div className="dk">dkdkdk</div>
+    </div>
+  );
+};
+export default CartList;
