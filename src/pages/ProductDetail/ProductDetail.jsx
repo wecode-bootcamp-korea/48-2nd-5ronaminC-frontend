61c2d2bd9dc6a8af7faf0f8da6cd345624f24146
serenity1091 <seulki1091@gmail.com>
2023-08-26 09:49:47 +0900

    Add : 제품 상세 페이지 레이아웃 완료

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 //import Nav from '../../components/Nav/Nav';
 //import Footer from '../../components/Footer/Footer';
 import ProductImages from './components/ProductImages';
-import ProductDetailInfo from './components/ProductDetailInfo';
-import ProductReview from './components/ProductReview';
-import ProductQnA from './components/ProductQnA';
+//import ProductDetailInfo from './components/ProductDetailInfo';
+//import ProductReview from './components/ProductReview';
+//import ProductQnA from './components/ProductQnA';
 import ProductNameInfo from './components/ProductNameInfo';
 
 import './ProductDetail.scss';
@@ -19,12 +19,33 @@ const ProductDetail = () => {
             <ProductImages />
           </div>
           <div className="productDetail">
-            <ProductDetailInfo />
-            <ProductReview />
-            <ProductQnA />
+            <button className="productDetailInfo">
+              <div className="productInfoName">
+                <p>제품 설명</p>
+              </div>
+              <div className="productInfoNameImg">
+                <img src="images/next.png" alt="제품 설명 버튼" />
+              </div>
+            </button>
+            <button className="productReview">
+              <div className="productReviewName">
+                <p>상품평</p>
+              </div>
+              <div className="productReviewNameImg">
+                <img src="images/next.png" alt="상품평 버튼" />
+              </div>
+            </button>
+            <button className="productQnA">
+              <div className="productQnAName">
+                <p>제품 Q&A</p>
+              </div>
+              <div className="productQnANameImg">
+                <img src="images/next.png" alt="제품 Q&A 버튼" />
+              </div>
+            </button>
           </div>
         </div>
-        <div className="productName">
+        <div className="productNames">
           <ProductNameInfo />
         </div>
       </div>
--- src/pages/ProductDetail/components/ProductNameInfo.jsx
+++ src/pages/ProductDetail/components/ProductNameInfo.jsx
@@ -3,8 +3,82 @@ import './ProductNameInfo.scss';
 
 const ProductNameInfo = () => {
   return (
-    <div className="ProductNameInfo">
-      <div className="dk">dkdkdk</div>
+    <div className="productNameInfo">
+      <div className="productNew">New</div>
+      <div className="productInfoTop">
+        <div className="productNameWrap">
+          <div className="productName">
+            <p>ProductName</p>
+          </div>
+          <div className="productItemDetail">
+            <div className="productItem">품목</div>,
+            <div className="productColor">색상</div>,
+          </div>
+          <div className="productLength">
+            <div className="productWidth">가로</div>X
+            <div className="productDepth">세로</div>X
+            <div className="productHeight">높이</div>
+          </div>
+          <div className="productPrice">
+            <p className="productMoney">2,344,563</p>
+            <p className="productPriceUnit">원</p>
+          </div>
+        </div>
+        <div className="productHeart">
+          <img src="images/heart.png" alt="찜하기 버튼" />
+        </div>
+        <div className="productReview">
+          <p>상품평</p>
+          <p>(</p>
+          <p>개수</p>
+          <p>)</p>
+        </div>
+      </div>
+      <div className="productRelated">
+        <div className="productDeliveryFee">
+          <img src="images/delivery-truck.png" alt="배송 아이콘" />
+          <p>배송비 : 10만원 이상 무료</p>
+          <button>자세히</button>
+        </div>
+        <div className="productShopInfo">
+          <img src="images/shop.png" alt="매장 아이콘" />
+          <p>매장 정보</p>
+        </div>
+        <div className="productAssembly">
+          <img src="images/diy.png" alt="조립 서비스 아이콘" />
+          <p>조립 서비스</p>
+        </div>
+      </div>
+      <div className="productOrder">
+        <div className="productQuantityPrice">
+          <div className="productQuantity">
+            <div className="productCount">
+              <input type="text" min="1" max="99" placeholder="1" />
+            </div>
+            <button className="productQuantityMinus">
+              <img src="images/decrease.png" alt="마이너스 아이콘" />
+            </button>
+            <button className="productQuantityPlus">
+              <img src="images/increase.png" alt="플러스 아이콘" />
+            </button>
+          </div>
+          <div className="productAll">
+            <p className="priceAllName">총 금액</p>
+            <p className="productAllPrice">2,344,563</p>
+            <p className="priceUnit">원</p>
+          </div>
+        </div>
+        <div className="productOrderButton">
+          <button className="productDirectOrder">
+            <img src="images/check-white.png" alt="바로구매 아이콘" />
+            <p>바로구매</p>
+          </button>
+          <button className="productCart">
+            <img src="images/add-to-cart-white.png" alt="장바구니 아이콘" />
+            <p>장바구니</p>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
