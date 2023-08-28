7017bbd1dad9d07ea1d166b04759626ae2da8773
serenity1091 <seulki1091@gmail.com>
2023-08-28 14:40:54 +0900

    Add : 장바구니  페이지 Layout 구현 완성

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,36 +1,30 @@
 import React from 'react';
-//import Nav from '../../components/Nav/Nav';
-// import Footer from '../../components/Footer/Footer';
 import CartList from './components/CartList';
 import ProductPriceAll from './components/ProductPriceAll';
 import './Cart.scss';
 
 const Cart = () => {
   return (
-    <>
-      {/* <Nav /> */}
-      <div className="cart">
-        <div className="cartAll">
-          <div className="cartName">
-            <div className="cartHeadName">
-              <h1>장바구니</h1>
-              <img src="images/shopping-cart.png" alt="장바구니 아이콘" />
-            </div>
-            <div className="cartQuantity">
-              <p className="cartQuantityName">전체 상품</p>
-              <p className="cartQuantitUnit">({`99`}개)</p>
-            </div>
+    <div className="cart">
+      <div className="cartAll">
+        <div className="cartName">
+          <div className="cartHeadName">
+            <h1>장바구니</h1>
+            <img src="images/shopping-cart.png" alt="장바구니 아이콘" />
           </div>
-          <div className="cartList">
-            <CartList />
+          <div className="cartQuantity">
+            <p className="cartQuantityName">전체 상품</p>
+            <p className="cartQuantitUnit">(99개)</p>
           </div>
         </div>
-        <div className="productPrice">
-          <ProductPriceAll />
+        <div className="cartList">
+          <CartList />
         </div>
       </div>
-      {/* <Footer /> */}
-    </>
+      <div className="productPrice">
+        <ProductPriceAll />
+      </div>
+    </div>
   );
 };
 export default Cart;
