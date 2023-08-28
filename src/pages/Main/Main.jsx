11d79e870daf6b002dce51eba37efdf2dde011f4
wanwork <wjdqo9705@gmail.com>
2023-08-28 16:38:56 +0900

    Add: 초기 파일 이동 완료

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,7 +2,34 @@ import React from 'react';
 import './Main.scss';
 
 const Main = () => {
-  return <>나는 메인</>;
+  return (
+    <div className="main">
+      <ul className="tabBar">
+        <li>거실</li>
+        <li>침실</li>
+        <li>주방</li>
+        <li>욕실</li>
+      </ul>
+
+      <div className="showRoomContainer">
+        <div className="imageContainer">
+          <img src="" alt="" />
+        </div>
+        <div className="listContainer">
+          <ul>
+            <li />
+            <li />
+            <li />
+            <li />
+            <li />
+            <li />
+            <li />
+            <li />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
