ddbfc6343bd8b2797cc1ece4f4d3f63c3377372a
serenity1091 <seulki1091@gmail.com>
2023-08-28 14:49:20 +0900

    Add : 결제  페이지 Layout 구현 완성

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,30 +1,24 @@
 import React from 'react';
-//import Nav from '../../components/Nav/Nav';
-// import Footer from '../../components/Footer/Footer';
 import UserInfoList from './components/UserInfoList';
 import PaymentProductPrice from './components/PaymentProductPrice';
 import './Payment.scss';
 
 const Payment = () => {
   return (
-    <>
-      {/* <Nav /> */}
-      <div className="payment">
-        <div className="paymentAll">
-          <div className="paymentHeadName">
-            <h1>주문 / 결제</h1>
-            <img src="images/delivery-truck.png" alt="배송 아이콘" />
-          </div>
-          <div className="UserInfoList">
-            <UserInfoList />
-          </div>
+    <div className="payment">
+      <div className="paymentAll">
+        <div className="paymentHeadName">
+          <h1>주문 / 결제</h1>
+          <img src="images/delivery-truck.png" alt="배송 아이콘" />
         </div>
-        <div className="paymentProductPrice">
-          <PaymentProductPrice />
+        <div className="UserInfoList">
+          <UserInfoList />
         </div>
       </div>
-      {/* <Footer /> */}
-    </>
+      <div className="paymentProductPrice">
+        <PaymentProductPrice />
+      </div>
+    </div>
   );
 };
 export default Payment;
