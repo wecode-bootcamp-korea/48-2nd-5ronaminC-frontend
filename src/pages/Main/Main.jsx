d993cf986f3c27639240d1c5ae84813ce94c95aa
wanwork <wjdqo9705@gmail.com>
2023-08-29 12:55:14 +0900

    Add: 서비스 레이아웃 추가

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,32 +1,65 @@
 import React from 'react';
+import './ServiceData';
 import './Main.scss';
 
 const Main = () => {
   return (
     <div className="main">
-      <ul className="tabBar">
-        <li>거실</li>
-        <li>침실</li>
-        <li>주방</li>
-        <li>욕실</li>
-      </ul>
+      <div className="mainVisual">
+        <h2>공간별 쇼핑하기</h2>
+        <ul className="tabBar">
+          <li>거실</li>
+          <li>침실</li>
+          <li>주방</li>
+          <li>욕실</li>
+        </ul>
+        <div className="showRoomContainer">
+          <div className="imageContainer">
+            <img
+              src="https://user-images.githubusercontent.com/77326740/263655844-aa9fdc4e-e0ca-4637-a07c-305e094d2308.jpg"
+              alt=""
+            />
+          </div>
+          <div className="listWireframe">
+            <ul className="listContainer">
+              <li>
+                <a className="dot" />
+              </li>
+              <li>
+                <a className="dot" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
 
-      <div className="showRoomContainer">
-        <div className="imageContainer">
-          <img src="" alt="" />
-        </div>
-        <div className="listWireframe">
-          <ul className="listContainer">
-            <li />
-            <li />
-            <li />
-            <li />
-            <li />
-            <li />
-            <li />
-            <li />
-          </ul>
-        </div>
+      <div className="serviceContainer">
+        <h2>다양한 이케아 서비스</h2>
+        <ul className="services">
+          <li>
+            <img src="/images/truck.png" alt="배송서비스" />
+            <span>배송 서비스</span>
+          </li>
+          <li>
+            <img src="/images/diy.png" alt="조립서비스" />
+            <span>조립 서비스</span>
+          </li>
+          <li>
+            <img src="/images/tool.png" alt="설치서비스" />
+            <span>설치 서비스</span>
+          </li>
+          <li>
+            <img
+              src="/images/interior-design.png"
+              alt="인테리어 디자인 서비스"
+            />
+            <span>인테리어 디자인 서비스</span>
+          </li>
+          <li>
+            <img src="/images/recycle-bag.png" alt="바이백 서비스" />
+            <span>바이백 서비스</span>
+          </li>
+        </ul>
       </div>
     </div>
   );
