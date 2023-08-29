e2b1e538506f0f52b7efa2251ccf1a8fae855f1b
serenity1091 <seulki1091@gmail.com>
2023-08-29 16:32:03 +0900

    Add : 장바구니 기능 구현 중

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -6,6 +6,21 @@ import './Cart.scss';
 const Cart = () => {
   const [cartData, setCartData] = useState({});
 
+  useEffect(() => {
+    fetch('http://10.58.52.244:3000/products/get7', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        authorization:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiaWF0IjoxNjkzMjkzMjM1LCJleHAiOjE2OTQxNTcyMzV9.LNetoMh3OgByppYadsnn8YOew6S9chO47kzaQOzQiG0',
+      },
+    })
+      .then(res => res.json())
+      .then(result => {
+        setCartData(result.data[0]);
+      });
+  }, []);
+
   return (
     <div className="cart">
       <div className="cartAll">
@@ -20,11 +35,11 @@ const Cart = () => {
           </div>
         </div>
         <div className="cartList">
-          <CartList />
+          <CartList cartData={cartData} />
         </div>
       </div>
       <div className="productPrice">
-        <ProductPriceAll />
+        <ProductPriceAll cartData={cartData} />
       </div>
     </div>
   );
