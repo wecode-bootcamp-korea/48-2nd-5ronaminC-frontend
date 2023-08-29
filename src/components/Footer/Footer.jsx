75d415c00b0b1bdecc878a4a6be94959216d8fe3
wanwork <wjdqo9705@gmail.com>
2023-08-29 20:59:09 +0900

    Add: Main Merge

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import './Footer.scss';
 
 const Footer = () => {
+  const { pathname } = useLocation();
+  const EXCEPTION_PATH = ['/', '/signup'];
+  const isExceptionPath = EXCEPTION_PATH.some(path => path === pathname);
+
+  if (isExceptionPath) return null;
+
   return <>나는 footer</>;
 };
 
