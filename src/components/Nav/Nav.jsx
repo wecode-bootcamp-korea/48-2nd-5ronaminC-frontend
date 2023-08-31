5acdbc00d949738ff6bd0d38603f6f982a56ce2e
wanwork <wjdqo9705@gmail.com>
2023-08-31 18:20:31 +0900

    Modify: logo 이미지 수정

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -46,7 +46,7 @@ const Nav = () => {
       <div className="firstLine">
         <img
           className="logo"
-          src="https://www.ikea.com/kr/ko/static/ikea-logo.f7d9229f806b59ec64cb.svg"
+          src="/images/5KEA-logo.jpg"
           alt="5KEA"
           onClick={goToMain}
         />
